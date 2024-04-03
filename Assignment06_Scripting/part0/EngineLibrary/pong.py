7e747efb7b02e8e58f6ce668c072160cbb55d134
Sam Zilli <samzilli01@gmail.com>
2024-04-03 12:29:31 -0400

    changed variables to constants

--- Assignment06_Scripting/part0/EngineLibrary/pong.py
+++ Assignment06_Scripting/part0/EngineLibrary/pong.py
@@ -1,10 +1,26 @@
 import mygameengine
 
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 400
+PADDLE_WIDTH = 10
+PADDLE_HEIGHT = 40
+BALL_WIDTH = 5
+BALL_HEIGHT = 5
+
 # Initialize SDL / window
-gameEngine = mygameengine.SDLGraphicsProgram(400, 400)
+gameEngine = mygameengine.SDLGraphicsProgram(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 # Main game loop
 quit = False
+
+
+
+class Paddle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 
 # While running 
 while not quit:
@@ -14,13 +30,13 @@ while not quit:
     gameEngine.clear()
 
     # left paddle
-    gameEngine.DrawRectangle(0, 200-40, 10, 40)
+    gameEngine.DrawRectangle(0, int((WINDOW_HEIGHT/2)-PADDLE_HEIGHT), PADDLE_WIDTH, PADDLE_HEIGHT)
 
     # right paddle
-    gameEngine.DrawRectangle(400-10, 200-40, 10, 40)
+    gameEngine.DrawRectangle(WINDOW_HEIGHT-PADDLE_WIDTH, int((WINDOW_HEIGHT/2)-PADDLE_HEIGHT), PADDLE_WIDTH, PADDLE_HEIGHT)
 
-    # ball?
-    gameEngine.DrawRectangle(200-10,200-10,10,10)
+    # ball
+    gameEngine.DrawRectangle(int(int((WINDOW_HEIGHT/2)-BALL_HEIGHT)), int(int((WINDOW_HEIGHT/2)-BALL_HEIGHT)), BALL_WIDTH, BALL_HEIGHT)
 
     # Delay for a short time
     gameEngine.delay(100)
