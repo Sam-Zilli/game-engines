655b63652bdd2c05c64e362182134258e52c0b92
Sam Zilli <samzilli01@gmail.com>
2024-04-19 19:53:12 -0400

    uncommenting out functions

--- finalproject/2Engine/include/GameApp.hpp
+++ finalproject/2Engine/include/GameApp.hpp
@@ -8,7 +8,6 @@
 
 // Forward declaration of GameObject class
 
-
 /// Game app
 class GameApp {
 public:
@@ -18,20 +17,33 @@ public:
     // void CreateGameObject(const std::string name, const std::string scriptfilename);
     // void Input();
     // void Update();
-    void Render();
-    void Loop();
+    // void Render();
+    // void Loop();
     void Poll();
-    void Clear();
+    // void Clear();
     void Delay(int milliseconds);
     void SetBackgroundColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a);
-    bool getQuit();
+    bool GetQuit();
     void Flip();
+    void DrawRectangle(int x, int y, int w, int h);
+    bool getRightPaddleUp();
+    bool getRightPaddleDown();
+    bool getLeftPaddleUp();
+    bool getLeftPaddleDown();
 
 private:
     // std::vector<GameObject> gameObjects;
     SDLGraphicsProgram graphicsProgram;
     bool isInitialized;
     bool quit;
+    // right paddle commands
+    bool rightPaddleUp = false;
+    bool rightPaddleDown = false;
+
+    // left paddle commands
+    bool leftPaddleUp = false;
+    bool leftPaddleDown = false;
+
 };
 
 #endif // GAMEAPP_HPP
