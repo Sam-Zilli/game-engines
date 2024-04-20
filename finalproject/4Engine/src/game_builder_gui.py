78ad23e4bb090e9c97396af5c60acd7fadcb5e45
Sam Zilli <samzilli01@gmail.com>
2024-04-20 15:26:27 -0400

    before attempting 2nd scene creation

--- finalproject/4Engine/src/game_builder_gui.py
+++ finalproject/4Engine/src/game_builder_gui.py
@@ -2,11 +2,28 @@ import mygameengine
 
 print("Before app creation")
 
+# Current working version
 values_dict = {
     "game_name": "Game Name!!!",
     "window_width": "1000",
     "window_height": "1000"
 }
+
+
+# More functionality version
+# values_dict_2 = {
+#     "game_name": "Game Name!!!",
+#     "window_width": "1000",
+#     "window_height": "1000",
+#     "backgroundRed: ": "0",
+#     "backgroundGreen: ": "0",
+#     "BackgroundBlue: ": "0",
+#     "backgroundAlpha: ": "255",
+#     "projectileSpeed: ": "10",
+#     "protagonistSpeed: ": "5",
+#     "numberOfEnemies: ": "2",
+# }
+
 
 loop_duration = 120.0
 
